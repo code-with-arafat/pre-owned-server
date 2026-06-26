c451d8afa7161b1173f1fb2141f3fe3a5e0770c0
code-with-arafat <ferdousinfo00@gmail.com>
2026-06-26 18:00:59 +0600

    Mongo URI Fixed

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const verifyToken = (req, res, next) => {
 };
 
 // MongoDB URI
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4lov0r3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
